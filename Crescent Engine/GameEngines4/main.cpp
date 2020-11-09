4969e0af2f6540faddd17bc4e6aa63b4e6400368
CrescentLua <johnanoya.com@gmail.com>
2020-11-09 13:03:20 -0500

    Added TinyXML2 to Project

--- Crescent Engine/GameEngines4/main.cpp
+++ Crescent Engine/GameEngines4/main.cpp
@@ -1,9 +1,15 @@
 #include "Engine/Core/CoreEngine.h"
+#include "Engine/Core/XMLParser/tinyxml2.h"
 #include "GAME/Game1.h"
+
+using namespace tinyxml2;
 
 int main(int argc, char* argv[])
 {
 	CoreEngine::GetInstance()->SetGameInterface(new Game1, RendererType::OPENGL);
+
+	//XMLDocument windowProperties; 
+	//windowProperties.LoadFile("./Resources/XMLFiles");
 
 	if (!CoreEngine::GetInstance()->OnCreate("Engine Window", 1600, 900)) 
 	{
